9980642bc8b248cb4730b1507be9e22a95a60c9c
deva101170 <145699994+deva101170@users.noreply.github.com>
2023-09-22 17:19:04 -0400

    Add files via upload

---
+++ PlayBlackJack.py
@@ -0,0 +1,277 @@
+#! /usr/bin/python
+
+''' 
+BlackJack card game play
+
+'''
+
+import random
+import sys
+
+suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
+ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
+values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8, 'Nine':9, 'Ten':10, 'Jack':10,
+         'Queen':10, 'King':10, 'Ace':11}
+
+playing = True
+
+#creating card class#
+
+class Card:
+    
+    def __init__(self, suit, rank):
+        self.suit = suit
+        self.rank = rank
+    
+    def __str__(self):
+        return self.rank + ' of ' + self.suit
+
+#creating Deck, shuffle function and single dealing#
+
+class Deck:
+    
+    def __init__(self):
+        self.deck = []  # start with an empty list#
+        for suit in suits:
+            for rank in ranks:
+                self.deck.append(Card(suit, rank))
+    
+    def __str__(self):
+        deck_comp = '' #starting competition deck empty#
+        for card in self.deck:
+            deck_comp += '\n' + card.__str__() #add each card object
+        return deck_comp
+            
+    def shuffle(self):
+        random.shuffle(self.deck)
+        
+    def deal(self):
+        single_card = self.deck.pop()
+        return single_card
+
+#creating a hand#
+
+class Hand:
+    def __init__(self):
+        self.cards = []  # start with an empty list as we did in the Deck class
+        self.value = 0   # start with zero value
+        self.aces = 0    # add an attribute to keep track of aces
+    
+    def add_card(self,card):
+        self.cards.append(card)
+        self.value += values[card.rank]
+        if card.rank == 'Ace':
+            self.aces += 1
+    
+    def adjust_for_ace(self):
+        while self.value > 21 and self.aces:
+            #print(self.value) 
+            self.value -= 10
+            self.aces -= 1
+            
+#creating Chips balance for competitor#            
+
+class Chips:
+    
+    def __init__(self):
+        self.total = 100  # This can be set to a default value or supplied by a user input
+        self.bet = 0
+        
+    def win_bet(self):
+        self.total += self.bet
+    
+    def lose_bet(self):
+        self.total -+ self.bet
+
+''' ************************************************************************************************************************************** '''
+        
+#Taking bets#
+
+def take_bet(chips):
+    while True:
+        try:
+            chips.bet = int(input('How many chips would you like to bet?  '))
+        except ValueError:
+            print("Sorry, a bet must be an integer!")
+        else:
+            if chips.bet > chips.total:
+                print('Sorry, your bet cannot exceed {} '.format(chips.total))
+            if chips.bet < 1:
+                print('Sorry, enter a value greater than 0 ')
+            else:
+                break      
+
+# taking hits#
+
+def hit(deck,hand):
+    hand.add_card(deck.deal())
+    if aceval == 0:
+    	hand.adjust_for_ace()
+
+#player to take hits or stand#
+
+def hit_or_stand(deck,hand):
+    global playing
+    
+    while True:
+        x = input("Would you like to Hit or Stand? Enter 'h' or 's'")
+        
+        if x[0].lower() == 'h':
+            hit(deck,hand)  # hit() function defined above
+
+        elif x[0].lower() == 's':
+            print("Player stands. Dealer is playing.")
+            playing = False
+
+        else:
+            print("Sorry, invalid entry. Please try again.")
+            continue
+        break
+
+#functions to display cards#
+
+def show_some(player,dealer):
+    print("\nDealer's Hand")
+    print("<card hidden>")
+    print(' ', dealer.cards[1])
+    print("\nPlayer's Hand: ", *player.cards, sep='\n')
+    print("")
+        
+def show_all(player,dealer):
+    print("\nDealer's Hand:", *dealer.cards, sep="\n")
+    print("Dealer's Hand =",dealer.value)
+    print("\nPlayer's Hand: ", *player.cards, sep='\n')
+    print("Player's Hand = ", player.value)
+
+#functions to handle game scenarios#
+
+def player_busts(player,dealer,chips):
+    print("\nPlayer busts!")
+    chips.lose_bet()
+
+def player_wins(player,dealer,chips):
+    print("\nPlayer wins!")
+    chips.win_bet()
+
+def dealer_busts(player,dealer,chips):
+    print("\nDealer busts!")
+    chips.win_bet()
+    
+def dealer_wins(player,dealer,chips):
+    print("\nDealer wins!")
+    chips.lose_bet()
+    
+def push(player,dealer):
+    print("\nDealer and Player tie! It's a push.")  
+
+#functions to set up game scenarios#
+
+def validateage():
+    ValidateAge = input('Are you 18 years or older? [Please enter "y" for Yes and "n" for No]: ') 
+    if ValidateAge == "y":
+       status="Eligible"
+    elif ValidateAge == "n":
+    	 status="Not Eligible"
+    	 print("You are ",status," to Play.")
+    	 sys.exit(0)
+    else:
+    	 print("Sorry invalid entry. Please try again")
+
+def acevalue():
+    global aceval
+    while True:
+        try:
+            aceval = int(input('What do you want the value of ACE to be? [Enter 1 OR 11.] [Enter 0 if you want the the game to automatically adjust the value]: '))
+        except ValueError:
+            print("Sorry, Input must be an integer!\n")
+        else:
+            if aceval > 0:
+                values['Ace'] = aceval
+            break   
+
+''' ************************************************************************************************************************************** '''
+
+#NOW FOR THE GAME
+
+#Verify age to play
+validateage()
+
+# Print an opening statement
+print("Let's play Blackjack!\n")
+
+while True:
+
+    # Prompt for value of ACE and append to dict
+    aceval = 11
+    acevalue()
+
+    # Create & shuffle the deck, deal two cards to each player
+    deck = Deck()
+    deck.shuffle()
+    
+    player_hand = Hand()
+    player_hand.add_card(deck.deal())
+    player_hand.add_card(deck.deal())
+    
+    dealer_hand = Hand()
+    dealer_hand.add_card(deck.deal())
+    dealer_hand.add_card(deck.deal())
+    
+    # Set up the Player's chips
+    player_chips = Chips()
+        
+    # Prompt the Player for their bet
+    take_bet(player_chips)
+    
+    # Show cards (but keep one dealer card hidden)
+    show_some(player_hand, dealer_hand)
+    
+    while playing:  # recall this variable from our hit_or_stand function
+        
+        # Prompt for Player to Hit or Stand
+        hit_or_stand(deck, player_hand)
+        
+        # Show cards (but keep one dealer card hidden)
+        show_some(player_hand,dealer_hand) 
+        
+        # If player's hand exceeds 21, run player_busts() and break out of loop
+        if player_hand.value >21:
+            player_busts(player_hand, dealer_hand, player_chips)
+            # Show all cards
+            show_all(player_hand,dealer_hand)
+            break
+
+    # If Player hasn't busted, play Dealer's hand until Dealer reaches 17
+    if player_hand.value <= 21:
+        
+        while dealer_hand.value <17:
+            hit(deck, dealer_hand)
+    
+        # Show all cards
+        show_all(player_hand,dealer_hand)
+        
+        # Run different winning scenarios
+        if dealer_hand.value > 21:
+            dealer_busts(player_hand,dealer_hand,player_chips)
+
+        elif dealer_hand.value > player_hand.value:
+            dealer_wins(player_hand,dealer_hand,player_chips)
+
+        elif dealer_hand.value < player_hand.value:
+            player_wins(player_hand,dealer_hand,player_chips)
+
+        else:
+            push(player_hand,dealer_hand)
+        
+    
+    # Inform Player of their chips total
+    print("\nPlayers winnings stand at", player_chips.total, "\n")
+    
+    # Ask to play again
+    new_game = input("would you like to play again? Enter 'y' or 'n'")
+    if new_game[0].lower() == 'y':
+        playing = True
+        continue
+    else:
+        print('Thanks for playing! ')
+        break
---
+++ README.md
@@ -0,0 +1,124 @@
+# Praeses Blackjack
+ BlackJack Game in Python
+
+Play the BlackJack Card game with one or more players.
+
+## Description
+The Blackjack game program consists of one or more players and a dealer.
+The player attempts to beat the dealer by getting a count as close to 21 as possible, without going over 21.
+An in-depth paragraph about your project and overview of use.
+
+## Getting Started
+
+### Dependencies
+python3.11.5 needed before installing program.
+
+* Describe any prerequisites, libraries, OS version, etc., needed before installing program.
+* ex. Windows 10 and above
+
+### Installing
+
+* How/where to download your program
+  https://www.python.org/downloads/
+* Any modifications needed to be made to files/folders
+  NA
+
+### Executing program
+
+* How to run the program
+How to run the program
+- Open a command line console
+- Go to the path where the source is copied
+- Type PlayBlackJack.py
+- Hit enter
+
+* Step-by-step bullets
+Game Play: Steps to play a hand
+Create a deck of 52 cards
+Shuffle the deck
+Ask the Player if they are over 18 years
+Ask the Player for the value of Ace to be used in that game/hand
+Ask the Player for their bet
+Make sure that the Player’s bet does not exceed their available chips
+Deal two cards to the Dealer and two cards to the Player
+Show only one of the Dealer’s cards, the other remains hidden
+Show both of the Player’s cards
+Ask the Player if they wish to Hit, and take another card
+If the Player’s hand doesn’t Bust (go over 21), ask if they’d like to Hit again.
+If a Player Stands, play the Dealer’s hand. The dealer will always Hit until the Dealer’s value meets or exceeds 17
+Determine the winner and adjust the Player’s chips accordingly
+Ask the Player if they’d like to play again
+
+The End Game
+When the dealer’s score is either 17 or more, we move onto the End Game, which involves comparing of values and nominating the winner of the game. There can be a few scenarios possible:
+
+The Dealer Busts – The dealer’s score exceeds 21.
+The Dealer has Blackjack – The dealer has an exact score of 21.
+The Player Busts – The player’s score exceeds 21.
+The Player has Blackjack – The player has an exact score of 21.
+A Tie Game – Both the player and the dealer has equal score.
+The Player Wins – The player’s score is more than that of the dealer.
+The Dealer Wins – The dealer’s score is more than that of the player.
+We check for each of the above possibilities and declare the winner.
+
+Classes used in the code:
+- Card Class: Has two attributes: suit and rank. Returns a string like “Two of Hearts”
+- Deck Class: Instantiate all 52 unique card objects and add them to the list. Has methods to shuffle the deck, and to deal out cards during gameplay.
+- Hand Class: Holds Card objects dealt from the Deck, the Hand class is used to calculate the value of those cards using the values dictionary defined in Deck. Has method to     	      adjust the value of Aces when appropriate.
+- Chips Class: Keeps track of a Player’s starting chips, bets, and ongoing winnings.
+
+Functions used in the code:
+- take_bet: Store the integer value of chips that is within the Player's betting limit.
+- hit:  Called during gameplay anytime a Player requests a hit or a Dealer’s hand is less than 17. Deck and Hand objects are arguments.	It will deal one card off the deck and 	add it to the Hand. It has the ability to check for aces in the event that a player’s hand exceeds 21, if the player does not choose a value for Aces.
+- hit_or_stand: Prompt the user for hit or stand. If the Player Hits, employ the hit() function. If the Player Stands, set the playing variable to False.
+- show_some: The Dealer’s first card is hidden and all of Player’s cards are visible in the first hand.
+- show_all: All of Dealer's and Player’s cards are visible at the last hand. Also shows each hand’s total value.
+- player_busts: When the Player's total value exceeds 21.
+- player_wins: When the Player's total value is more than the Dealer's.
+- dealer_busts: When the Dealer's total value exceeds 21.
+- dealer_wins: When the Dealer's total value is more than the Player's.
+- push: When the Dealer's and the Player's total value is the same aka a tie.
+
+
+Adiditional features:
+Prompt for age to validate if eligible to play BlackJack.
+Prompt for value of Ace.
+Prompt for Chips and check it does not exceed the maximum limit and is greater than 0.
+If Ace value is not selected it defaults to adjust_for_aces method that decreases the number of aces to make an adjustment to stay under 21.
+
+
+## Help
+
+Any advise for common problems or issues.
+Hit ctrl+c to end the program.
+Run the program again.
+
+command to run if program contains helper info
+NA
+
+## Authors
+Devamani Francis
+
+Contributors names and contact info
+NA
+
+## Version History
+Versio 1.0 Intitial Release
+
+* 0.2
+    * Various bug fixes and optimizations
+    * See [commit change]() or See [release history]()
+* 0.1
+    * Initial Release
+
+## License
+
+This project is licensed under the [NAME HERE] License - see the LICENSE.md file for details
+NA
+
+## Acknowledgments
+
+Inspiration, code snippets, etc.
+* [awesome-gameplaydetails](https://bicyclecards.com/how-to-play/blackjack/)
+* [simple-readme](https://gist.github.com/DomPizzie/7a5ff55ffa9081f2de27c315f5018afc#file-readme-template-md)
+
